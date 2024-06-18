e78f9c1d84cd79818b470260212edad4c126a0a7
chalolowa <wangwelocha@students.uonbi.ac.ke>
2024-06-18 23:02:40 +0300

    On branch main  Fixed routing bugs  Changes to be committed: 	modified:   frontend/src/pages/Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate = useNavigate();
+  
+  const handleClick = () => {
+    navigate('/register');
+  }
+
   return (
     <div>
       <div>
         <h1>Roadside Rescue Assistance</h1>
         <p>Your reliable and professional roadside assistance service on Kenyan highways.</p>
-        <button>Get help</button>
+        <button onClick={handleClick}>Get help</button>
       </div>
     </div>
   )
